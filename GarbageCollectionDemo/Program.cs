7e05d476ac259cbfa10ea245c5fdeeefbd93c751
Roy Lovitt <lovittl@my.erau.edu>
2018-02-23 14:08:24 -0500

    add completed program to demo Garbage Collection  with error handling.

--- GarbageCollectionDemo/Program.cs
+++ GarbageCollectionDemo/Program.cs
@@ -10,6 +10,13 @@ namespace GarbageCollectionDemo
     {
         static void Main(string[] args)
         {
+            using (Calculator calculator = new Calculator())
+            {
+                //Domonstration of Garbage Control when exception thrown.
+                Console.WriteLine($"120 / 0 = {calculator.Divide(120, 0)}");
+                //Console.WriteLine($"120 / 15 = {calculator.Divide(120, 15)}");
+            }
+            Console.WriteLine("Program finishing");
         }
     }
 }
